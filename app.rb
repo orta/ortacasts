1dd91b85e56dc72db845a4c6bbc721a963fd27e1
orta <orta.therox@gmail.com>
2011-04-01 12:14:45 +0100

    styled!

--- app.rb
+++ app.rb
@@ -5,21 +5,26 @@ require 'cgi'
 
        
 get "/" do
+  get_items
   erb :index
 end
 
 get "/rss" do
-  
-@items = [
-        { :url => "amazon", :title => "first podcasst", 
-          :description => "bit more info" , 
-          :date => "Fri, 09 Sep 2005 00:00:00 -0000",
-          :length => "343434" },
-          
-          { :url => "amazon", :title => "second podcasst", 
+  get_items
+  erb :rss
+end
+
+def get_items
+  @items = [
+          { :link => "amazon", :title => "first podcasst", 
             :description => "bit more info" , 
             :date => "Fri, 09 Sep 2005 00:00:00 -0000",
-            :length => "343434" }
-       ]
-  erb :rss
+            :length => "343434" },
+
+            { :link => "amazon", :title => "second podcasst", 
+              :description => "bit more info" , 
+              :date => "Fri, 09 Sep 2005 00:00:00 -0000",
+              :length => "343434" }
+         ]
+   @items=[]
 end
