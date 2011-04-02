2006e4d581efb66ffe5e56fe49805b503aae1404
orta <orta.therox@gmail.com>
2011-04-03 00:27:37 +0100

    Added some tests

--- tests.rb
+++ tests.rb
@@ -1,9 +1,16 @@
 require 'data'
+require 'cgi'
 
-get_data
+get_items()
 
 @items.each do |cast| 
   
-  "no link on " unless cast[:link] 
-  "no "  unless cast[:something]
+  # I want to know all the attributes are set
+  attrs = %w( title sub link  description date)
+  attrs.each do | attribute |
+    puts "no #{attribute} on #{ CGI.unescapeHTML(cast[:title]) }"  unless cast[attribute.to_sym]
+  end
+  
+  # I want to check the file exists
+  # I want to check that there' no duplication on the feeds
 end
