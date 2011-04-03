582d1824ad8c3f64d773c7ab430d7675e548f5f9
orta <orta.therox@gmail.com>
2011-04-03 23:59:23 +0100

    episode 3

--- data.rb
+++ data.rb
@@ -1,9 +1,10 @@
 def get_items
+  @server = "https://ortacasts.s3.amazonaws.com/"
   @items = [
     
           { 
             :title => "Grax breaks orta in", 
-            :link => "https://ortacasts.s3.amazonaws.com/podcast001.mp3", 
+            :link => "podcast001.mp3", 
             :sub => "cohost: Grax of graxdomain.co.uk",
             :description => "talking a bit about what inspired the project, whether it will produce anything of interest, and the current setup. It's a quickie. Cause it was my first time." , 
             :date => "Fri, 01 April",
@@ -11,12 +12,19 @@ def get_items
 
             { 
               :title => "&quot;It&#x27;s just hard to get things started&quot; &quot;well let me stop you there&quot;", 
-              :link => "https://ortacasts.s3.amazonaws.com/podcast002.mp3", 
+              :link => "podcast002.mp3", 
               :sub => "cohost: Grax of graxdomain.co.uk",
               :description => "Setting up a podcast, whether I have the rights to use the music in the background, grax's experience with a photo shoot, orta getting naked under a waterfall, iPad for technophobes, Grax hates iTunes and wishes he could see his naked photos.", 
               :date => "Sat, 02 April",
-            }
+            },
             
+            { 
+              :title => "guys are a piece of cake", 
+              :link => "podcast003.mp3", 
+              :sub => "",
+              :description => 'it was mothers day today, and I represented. I then talk about the Back to the Future games. Which you can try for <a href="http://www.telltalegames.com/bttf">free here</a>', 
+              :date => "Sun, 03 April",
+            }
             
           ]
 end
--- tests.rb
+++ tests.rb
@@ -1,8 +1,11 @@
-require 'data'
+require File.dirname(__FILE__) + '/data'
 require 'cgi'
+require 'uri'
+require 'net/http'
 
 get_items()
 
+links = []
 @items.each do |cast| 
   
   # I want to know all the attributes are set
@@ -11,6 +14,16 @@ get_items()
     puts "no #{attribute} on #{ CGI.unescapeHTML(cast[:title]) }"  unless cast[attribute.to_sym]
   end
   
-  # I want to check the file exists
+  # # I want to check the file exists
+  # uri = URI.parse( @server + cast[:link] )
+  # http = Net::HTTP::new(uri.host, uri.port)
+  # response = http.request_head( cast[:link] )
+  # puts "location doenst exist on #{ CGI.unescapeHTML(cast[:title]) }" unless response.is_a? Net::HTTPSuccess
+
   # I want to check that there' no duplication on the feeds
+  if( links.index(cast[:link]) ) 
+    puts "duped location on #{ CGI.unescapeHTML(cast[:title]) }" 
+  end
+  links.push cast[:link]
+  
 end
