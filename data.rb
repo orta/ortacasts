5aff7d2695f70304a30d03f7954de6e9a941bcca
orta <orta.therox@gmail.com>
2011-04-04 20:20:42 +0100

    more tags, duration, episode 4

--- data.rb
+++ data.rb
@@ -2,12 +2,13 @@ def get_items
   @server = "https://ortacasts.s3.amazonaws.com/"
   @items = [
     
-          { 
-            :title => "Grax breaks orta in", 
-            :link => "podcast001.mp3", 
-            :sub => "cohost: Grax of graxdomain.co.uk",
-            :description => "talking a bit about what inspired the project, whether it will produce anything of interest, and the current setup. It's a quickie. Cause it was my first time." , 
-            :date => "Fri, 01 April",
+           { 
+              :title => "Grax breaks orta in", 
+              :link => "podcast001.mp3", 
+              :sub => "cohost: Grax of graxdomain.co.uk",
+              :description => "Talking a bit about what inspired the project, whether it will produce anything of interest, and the current setup. It's a quickie. Cause it was my first time." , 
+              :date => "Fri, 01 April",
+              :length => "8:37"
            },
 
             { 
@@ -16,15 +17,27 @@ def get_items
               :sub => "cohost: Grax of graxdomain.co.uk",
               :description => "Setting up a podcast, whether I have the rights to use the music in the background, grax's experience with a photo shoot, orta getting naked under a waterfall, iPad for technophobes, Grax hates iTunes and wishes he could see his naked photos.", 
               :date => "Sat, 02 April",
+              :length => "23:15"
             },
             
             { 
               :title => "guys are a piece of cake", 
               :link => "podcast003.mp3", 
               :sub => "",
-              :description => 'it was mothers day today, and I represented. I then talk about the Back to the Future games. Which you can try for <a href="http://www.telltalegames.com/bttf">free here</a>', 
+              :description => 'It was mothers day today, and I represented. I then talk about the Back to the Future games. Which you can try for <a href="http://www.telltalegames.com/bttf">free here</a>', 
               :date => "Sun, 03 April",
+              :length => "4:47"
+            },
+            
+            { 
+              :title => "each shot over laps a litttle", 
+              :link => "podcast004.mp3", 
+              :sub => "",
+              :description => "The cameras I've owned, why I developed a style and a photo website I've been working", 
+              :date => "Sun, 03 April",
+              :length => "10:25"
             }
+            
             
           ]
 end
